949b04561efa413a3f941e2bc191fb666cf1e610
N0rbert <309456+N0rbert@users.noreply.github.com>
2022-01-01 19:11:05 +0300

    draft fix with both gtk3-classic and ubuntu patches

--- scripts/create-ubuntu-package.sh
+++ scripts/create-ubuntu-package.sh
@@ -13,7 +13,7 @@ cd "$(dirname "$0")/../"
 # TODO: Add check it's running on Ubuntu
 
 # TODO: Get source files
-sudo apt-get source gtk+3.0
+apt-get source gtk+3.0
 
 # TODO: Check/prompt that source (deb-src) repositories are enabled in Software & Updates
 
@@ -22,22 +22,28 @@ sudo apt-get build-dep gtk+3.0
 sudo apt-get install build-essential quilt devscripts
 
 # TODO: Copy our patches/files to debian/patches, and update 'series' file
+cd gtk+3.0-3.24.30
+cp -v ../*.patch debian/patches
+cat ../series >> debian/patches/series
 
-# Apply patches before build
-ln -s debian/patches .
-quilt push -a
+sed -i "s/^appearance__message-dialogs.patch/#appearance__message-dialogs.patch/g" debian/patches/series
+sed -i "s/^csd__clean-headerbar.patch/#csd__clean-headerbar.patch/g" debian/patches/series
+sed -i "s/^csd__disabled-by-default.patch/#csd__disabled-by-default.patch/g" debian/patches/series
+sed -i "s/^file-chooser__places-sidebar.patch/#file-chooser__places-sidebar.patch/g" debian/patches/series
+
+#quilt push -a
 
 # Overwrite changelog
 CODENAME=$(lsb_release -c | awk '{print $2}')
 USERNAME=$(whoami)
 EMAIL="$USERNAME@$(cat /etc/hostname)"
-DATE=$(date +%a,\ %d\ %b\ %Y\ %H:%M:%S\ %z)
+DATE=$(LC_ALL=C date +%a,\ %d\ %b\ %Y\ %H:%M:%S\ %z)
 echo """
-gtk+3.0 (3.24.30-classic1) ${CODENAME}; urgency=low
+gtk+3.0 (3.24.30-1ubuntu1-classic1) ${CODENAME}; urgency=low
 
   * gtk3-classic build
 
- -- ${USERNAME} <${USERNAME}@${EMAIL}>  ${DATE}
+ -- ${USERNAME} <${EMAIL}>  ${DATE}
 """ > debian/changelog
 
 # TODO: Remove tests?
@@ -46,7 +52,7 @@ gtk+3.0 (3.24.30-classic1) ${CODENAME}; urgency=low
 # Create package!
 export DEB_BUILD_OPTIONS=nocheck
 #debuild -b
-dpkg-buildpackage -rfakeroot -b  #-uc
+dpkg-buildpackage -rfakeroot -b -uc
 
 # Install patched packages
 sudo apt install ../*.deb
